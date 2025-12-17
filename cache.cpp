6aa411fec8744ce8f6613672338094790c06e754
Ilay Bell <ilay.ben@campus.technion.ac.il>
2025-12-17 14:03:33 +0200

    we try

--- cache.cpp
+++ cache.cpp
@@ -125,7 +125,8 @@ void CacheManager::read(uint64_t addr) {
         this->l1.add_miss();
 
         this->l2.add_access();
-        if (this->l2.is_exist_in_set(set2, tag2)) {
+        Block* l2_block = this->l2.get_block_in_set(set2, tag2);
+        if (l2_block) {
             this->l2.update_LRU(set2, tag2);
         } else {
             this->l2.add_miss();
@@ -307,7 +308,8 @@ void CacheManager::write(uint64_t addr) {
         this->l1.add_miss();
 
         this->l2.add_access();
-        if (this->l2.is_exist_in_set(set2, tag2)) {
+        Block* l2_block = this->l2.get_block_in_set(set2, tag2);
+        if (l2_block) {
             if(this->is_write_allocate) {
                 this->l1.propogate_block(set1, tag1, addr, this);
                 this->l1.write_data(set1, tag1);
